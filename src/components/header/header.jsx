f57ed2aa13042bd54bc508c95d40f8d88ad5d2f1
kay7777 <kay7song@gmail.com>
2020-01-12 20:01:13 -0700

    fix user system

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { auth } from '../../firebase/firebase';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-const Header = () => (
+const Header = ({currentUser}) => (
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="/">Navbar</a>
+        <a className="navbar-brand" href="/">ShareSoul</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
             <a className="nav-item nav-link active" href="/user">My</a>
-            <a className="nav-item nav-link" href="/signin">SignIn</a>
+            {currentUser ? 
+            (<a className="nav-item nav-link" href="/signin" onClick={() => auth.signOut()}>LogOut</a>)
+            :
+            (<a className="nav-item nav-link" href="/signin">SignIn</a>)
+            }
             {/* <a class="nav-item nav-link" href="#">Pricing</a> */}
             </div>
         </div>
@@ -18,4 +26,8 @@ const Header = () => (
     </div>
 )
 
-export default Header;
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser
+});
+  
+export default connect(mapStateToProps)(Header);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-// import { Provider } from 'react-redux';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-// import { store, persistor } from './redux/store';
+import { store, persistor } from './redux/store';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-//   <Provider store={store}>
+  <Provider store={store}>
     <BrowserRouter>
-      {/* <PersistGate persistor={persistor}> */}
+      <PersistGate persistor={persistor}>
         <App />
-      {/* </PersistGate> */}
+      </PersistGate>
     </BrowserRouter>
-//   </Provider>
+  </Provider>
 ,
   document.getElementById('root')
 );
